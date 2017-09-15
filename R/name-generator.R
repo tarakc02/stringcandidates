feea665a8acb6237278b577513dbfbe1a767dda4
tarakc02 <tarakc02@github.com>
2017-09-15 09:45:21 -0700

    first-name generator (for testing)

---
+++ R/name-generator.R
@@ -0,0 +1,57 @@
+conditional_distribution <- function(context_size = 3L) {
+    name_data <- babynames::babynames %>%
+        dplyr::mutate(name = stringr::str_to_lower(name)) %>%
+        dplyr::group_by(sex, name) %>%
+        dplyr::summarise(n = sum(n)) %>%
+        dplyr::ungroup()
+
+    param_n <- context_size + 1
+    pad <- paste(rep(" ", context_size), collapse = "")
+
+    nm <- name_data %>%
+        dplyr::mutate(name = paste0(pad, name, "#"))
+
+    # the n-grams are the "context" along with the following letter
+    ngrams <- nm %>%
+        dplyr::mutate(ngram = tokenizers::tokenize_character_shingles(
+            name, n = param_n, strip_non_alphanum = FALSE
+        )) %>% tidyr::unnest() %>%
+        dplyr::group_by(sex, ngram) %>%
+        dplyr::summarise(cnt = sum(n))
+
+    # finally, i calculate the distribution over the last letter,
+    # given the context_size letters that came before
+    conditional_dist <- ngrams %>%
+        dplyr::mutate(prev = stringr::str_sub(ngram, 1, context_size),
+                      nxt = stringr::str_sub(ngram, param_n, param_n)) %>%
+        dplyr::group_by(sex, prev, nxt) %>%
+        dplyr::summarise(joint_count = sum(cnt)) %>%
+        dplyr::mutate(pct = joint_count / sum(joint_count)) %>%
+        dplyr::ungroup() %>%
+        dplyr::select(-joint_count)
+
+    conditional_dist %>%
+        dplyr::group_by(sex, prev) %>%
+        tidyr::nest() %>%
+        dplyr::mutate(
+            data = purrr::map(data,
+                              ~list2env(structure(as.list(.$pct), names = .$nxt)))) %>%
+        split(.$sex) %>%
+        purrr::map(~split(., .$prev)) %>%
+        purrr::map(list2env, hash = TRUE)
+}
+
+generator <- function(context_size, weight) {
+    res <- purrr::map(context_size, conditional_distribution)
+    structure(res, context_size = context_size, weight = weight)
+}
+
+gen_name <- function(nm, sex, n = 1) {
+    res <- replicate(
+        n,
+        interp(nm, sex, starter = "   ",
+               context_sizes = attr(nm, "context_size"),
+               weights = attr(nm, "weight"))
+    )
+    stringr::str_trim(res)
+}
---
+++ src/gen.cpp
@@ -0,0 +1,67 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+Environment lookup_env(List cond, std::string sex, std::string starter) {
+    Environment result = new_env();
+    List listresult;
+    List dfresult;
+    Environment bysex = cond[sex];
+
+    if (!bysex.exists(starter)) {
+        result.assign("#", 0);
+    } else {
+        dfresult = bysex[starter];
+        listresult = dfresult["data"];
+        result = listresult[0];
+    }
+    return result;
+}
+
+// [[Rcpp::export]]
+std::string interp(List conds, std::string sex, std::string starter,
+                   NumericVector context_sizes,
+                   NumericVector weights) {
+    std::string result = starter;
+    std::string nc = "";
+
+    while (nc != "#" && result.length() <= 7) {
+        Environment dist = new_env();
+        result = result.append(nc);
+        for (int i = 0; i < conds.size(); i++) {
+            std::string start = result.substr(result.size() - context_sizes[i]);
+            Environment this_dist = lookup_env(conds[i], sex, start);
+            double this_weight = weights[i];
+            CharacterVector this_keys = this_dist.ls(false);
+
+            for (int k = 0; k < this_keys.size(); k++) {
+                std::string key = std::string(this_keys[k]);
+                double this_pct = this_dist[key];
+                if (dist.exists(key))
+                    dist[key] = double(dist[key]) + this_weight * this_pct;
+                else dist[key] = this_weight * this_pct;
+            }
+        }
+
+        double rnd = R::runif(0,1);
+        double sum_so_far = 0;
+        CharacterVector candidates = dist.ls(false);
+
+        for (int cand = 0; cand < candidates.size() && sum_so_far < rnd; cand++) {
+            std::string this_candidate = std::string(candidates[cand]);
+            double this_prob = Rcpp::as<double>(dist.get(this_candidate));
+            sum_so_far += this_prob;
+            if (rnd <= sum_so_far) nc = this_candidate;
+        }
+    }
+    return result;
+}
+
+// CharacterVector csample_char( CharacterVector x,
+//                               int size,
+//                               bool replace,
+//                               NumericVector prob = NumericVector::create()
+// ) {
+//     CharacterVector ret = RcppArmadillo::sample(x, size, replace, prob) ;
+//     return ret ;
+// }
